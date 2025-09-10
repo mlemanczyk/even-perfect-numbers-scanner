d66c70e530964ee634c19f90603535decbf391aa
Marek Lemańczyk <marek@lemanczyk.eu>
2025-09-11 00:57:24 +0200

    Primes limit restored

--- PerfectNumbers.Core/PerfectNumberConstants.cs
+++ PerfectNumbers.Core/PerfectNumberConstants.cs
@@ -3,6 +3,6 @@ namespace PerfectNumbers.Core;
 public static class PerfectNumberConstants
 {
 	public const ulong BiggestKnownEvenPerfectP = 136279841UL;
-	public static readonly uint PrimesLimit = 500_000_000; //(ulong)Array.MaxLength;// 1_000_000;
+	public static readonly uint PrimesLimit = 1_000_000; //(ulong)Array.MaxLength;// 1_000_000;
     public const int MaxQForDivisorCycles = 4_000_000;
 }
