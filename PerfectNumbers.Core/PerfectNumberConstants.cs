fda058cde037ebd4190417a6d773f5cae877dc2b
Marek Lemańczyk <marek@lemanczyk.eu>
2025-09-10 21:13:10 +0200

    Tiny cleanup

--- PerfectNumbers.Core/PerfectNumberConstants.cs
+++ PerfectNumbers.Core/PerfectNumberConstants.cs
@@ -3,9 +3,6 @@ namespace PerfectNumbers.Core;
 public static class PerfectNumberConstants
 {
 	public const ulong BiggestKnownEvenPerfectP = 136279841UL;
-	public static readonly uint PrimesLimit = 1_000_000; //(ulong)Array.MaxLength;// 1_000_000;
-
-	// Maximum K for residue-based Mersenne test (q = 2*p*k + 1 up to this k)
-	public static readonly UInt128 ResidueMaxK = 5_000_000UL;
+	public static readonly uint PrimesLimit = 500_000_000; //(ulong)Array.MaxLength;// 1_000_000;
     public const int MaxQForDivisorCycles = 4_000_000;
 }
