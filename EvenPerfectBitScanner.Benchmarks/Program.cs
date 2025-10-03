874ea10a0e53192623fba260f42f2917a5371120
Marek Lemańczyk <marek@lemanczyk.eu>
2025-10-03 19:32:02 +0200

    Code for profiling added

--- EvenPerfectBitScanner.Benchmarks/Program.cs
+++ EvenPerfectBitScanner.Benchmarks/Program.cs
@@ -7,8 +7,23 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-		var config = DefaultConfig.Instance.WithOptions(ConfigOptions.JoinSummary);
+        var config = DefaultConfig.Instance.WithOptions(ConfigOptions.JoinSummary);
         BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
+
+        // Pow2MontgomeryModCycleComputationBenchmarks benchmarks = new()
+        // {
+        //     Scale = Pow2MontgomeryModCycleComputationBenchmarks.InputScale.Large
+        // };
+
+        // benchmarks.Setup();
+
+        // Console.WriteLine("Press ENTER to run");
+        // Console.ReadLine();
+
+        // foreach (int i in Enumerable.Range(0, 100_000))
+        // {
+        //     benchmarks.MontgomeryWithPrecomputedCycle();
+        // }
     }
 }
 
