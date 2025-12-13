079b2d5b72f2c22910eed04ff0fd4483b241c94d
mlemanczyk <marek@lemanczyk.eu>
2025-12-13 14:26:55 +0100

    PartialFactorPendingEntry changed back to struct for better performance.

--- PerfectNumbers.Core/PartialFactorPendingEntry.cs
+++ PerfectNumbers.Core/PartialFactorPendingEntry.cs
@@ -2,37 +2,9 @@ using System.Runtime.CompilerServices;
 
 namespace PerfectNumbers.Core;
 
-public sealed class PartialFactorPendingEntry(ulong value, bool knownComposite)
+public struct PartialFactorPendingEntry(ulong value, bool knownComposite)
 {
-	[ThreadStatic]
-	private static PartialFactorPendingEntry? _poolHead;
-	private PartialFactorPendingEntry? _next;
-
-	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-	public static PartialFactorPendingEntry Rent(ulong value, bool knownComposite)
-	{
-		var poolHead = _poolHead;
-		if (poolHead == null)
-		{
-			return new(value, knownComposite);
-		}
-
-		_poolHead = poolHead._next;
-		poolHead.Value = value;
-		poolHead.KnownComposite = knownComposite;
-		poolHead.HasKnownPrimality = knownComposite;
-		poolHead.IsPrime = false;
-		return poolHead;
-	}
-
-	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-	public static void Return(PartialFactorPendingEntry entry)
-	{
-		entry._next = _poolHead;
-		_poolHead = entry;
-	}
-
-	public ulong Value = value;
+	public readonly ulong Value = value;
 	public bool KnownComposite = knownComposite;
 	public bool HasKnownPrimality = knownComposite;
 	public bool IsPrime = false;
